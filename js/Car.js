982125223eb3c8a9f7f8980bd1b92316f49a15db
Nhat Khanh Huy Tran <khanhhuy288@gmail.com>
2017-06-01 00:02:20 +0200

    use prototype to define Car's functions

--- js/Car.js
+++ js/Car.js
@@ -23,71 +23,70 @@ function Car(whichImage) {
   this.keyHeld_TurnLeft = false;
   this.keyHeld_TurnRight = false;
   this.keyHeld_Paint = false;
-
-  this.setupInput = function (upKey, rightKey, downKey, leftKey, paintKey) {
-    this.controlKeyUp = upKey;
-    this.controlKeyRight = rightKey;
-    this.controlKeyDown = downKey;
-    this.controlKeyLeft = leftKey;
-    this.contolKeyPaint = paintKey;
-  };
-
-  this.move = function () {
-    // decrease 0.94 speed
-    this.speed *= GROUNDSPEED_DECAY_MULT;
-
-    // increase speed
-    if (this.keyHeld_Gas) {
-      this.speed += DRIVE_POWER;
-    }
-
-    // decrease speed
-    if (this.keyHeld_Reverse) {
-      this.speed -= REVERSE_POWER;
-    }
-
-    // disallow steering when speed is too low
-    if (Math.abs(this.speed) > MIN_SPEED_TO_TURN) {
-      if (this.keyHeld_TurnLeft) {
-        this.ang -= TURN_RATE;
-      }
-
-      if (this.keyHeld_TurnRight) {
-        this.ang += TURN_RATE;
-      }
-    }
-
-    // get car's position
-    this.x += Math.cos(this.ang) * this.speed;
-    this.y += Math.sin(this.ang) * this.speed;
-
-    // hit the bound
-    if (this.x < 0) {
-      this.x = canvasWidth;
-    }
-
-    if (this.x > canvasWidth) {
-      this.x = 0;
-    }
-
-    if (this.y < 0) {
-      this.y = canvasHeight;
-    }
-
-    if (this.y > canvasHeight) {
-      this.y = 0;
-    }
-  };
-
-  this.draw = function () {
-    drawBitmapCenteredWithRotation(this.myCarPic, this.x, this.y, this.ang);
-  };
-
-  this.paint = function () {
-    if (this.keyHeld_Paint) {
-      // paint the trail
-      colorCircle(this.x, this.y, 11, this.paintColor);
-    }
-  };
-
 }
+
+Car.prototype.setupInput = function (upKey, rightKey, downKey, leftKey, paintKey) {
+  this.controlKeyUp = upKey;
+  this.controlKeyRight = rightKey;
+  this.controlKeyDown = downKey;
+  this.controlKeyLeft = leftKey;
+  this.contolKeyPaint = paintKey;
+};
+
+Car.prototype.move = function () {
+  // decrease 0.94 speed
+  this.speed *= GROUNDSPEED_DECAY_MULT;
+
+  // increase speed
+  if (this.keyHeld_Gas) {
+    this.speed += DRIVE_POWER;
+  }
+
+  // decrease speed
+  if (this.keyHeld_Reverse) {
+    this.speed -= REVERSE_POWER;
+  }
+
+  // disallow steering when speed is too low
+  if (Math.abs(this.speed) > MIN_SPEED_TO_TURN) {
+    if (this.keyHeld_TurnLeft) {
+      this.ang -= TURN_RATE;
+    }
+
+    if (this.keyHeld_TurnRight) {
+      this.ang += TURN_RATE;
+    }
+  }
+
+  // get car's position
+  this.x += Math.cos(this.ang) * this.speed;
+  this.y += Math.sin(this.ang) * this.speed;
+
+  // hit the bound
+  if (this.x < 0) {
+    this.x = canvasWidth;
+  }
+
+  if (this.x > canvasWidth) {
+    this.x = 0;
+  }
+
+  if (this.y < 0) {
+    this.y = canvasHeight;
+  }
+
+  if (this.y > canvasHeight) {
+    this.y = 0;
+  }
+};
+
+Car.prototype.draw = function () {
+  drawBitmapCenteredWithRotation(this.myCarPic, this.x, this.y, this.ang);
+};
+
+Car.prototype.paint = function () {
+  if (this.keyHeld_Paint) {
+    // paint the trail
+    colorCircle(this.x, this.y, 11, this.paintColor);
+  }
+};
